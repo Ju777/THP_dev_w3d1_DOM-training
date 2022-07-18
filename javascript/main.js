b1bfa62903f97f0a5a7f1c604c4c6c00acf497f3
Julien Armagnac <julien.tamil@gmail.com>
2022-07-18 14:19:45 +0200

    Add a drag & drop feature .

--- javascript/main.js
+++ javascript/main.js
@@ -51,14 +51,15 @@ btnRandom.addEventListener('click', () => {
 const popup = document.getElementById('modal');
 const mainContainer = document.getElementById('main-container');
 
-document.body.addEventListener('mouseleave', () =>{
-    popup.style.display = 'block';
-    mainContainer.style.filter = "grayscale(75%)";
-    document.body.addEventListener('click', () => {
-                popup.style.display = 'none';
-                mainContainer.style.filter = "grayscale(0%)";  
-    });
-});
+// A REACTIVER A LA FIN DU PROJET
+// document.body.addEventListener('mouseleave', () =>{
+//     popup.style.display = 'block';
+//     mainContainer.style.filter = "grayscale(75%)";
+//     document.body.addEventListener('click', () => {
+//                 popup.style.display = 'none';
+//                 mainContainer.style.filter = "grayscale(0%)";  
+//     });
+// });
 
 const popupClosingLink = document.getElementById('modal-close');
 popupClosingLink.addEventListener('click', () =>{
@@ -69,5 +70,53 @@ popupClosingLink.addEventListener('click', () =>{
 // FIN
 
 // GALLERIE DE PHOTOS
+    // console.log(test.clientWidth );
+    // const bounds = test.getBoundingClientRect();
+    // console.log(bounds);
+    // console.log(bounds.height);
+
+const galleryContainer = document.getElementById('gallery-container');
+const allImages = document.getElementsByTagName('img');
+let draggedImage;
+let targetImage;
+
+// Tout d'abord : récupérer la photo sur laquelle on clique
+galleryContainer.addEventListener('mousedown', (e) => {
+    for(image of allImages) {
+        if(e.clientX >= image.x && e.clientX <= image.x + image.width && e.clientY >= image.y && e.clientY <= image.y + image.height) {
+            draggedImage = image;
+            // log de vérif
+            // console.log(draggedImage);
+        }
+    }
+})
+
+// Ensuite : récupérer les coordonnées de la zone de fin de drag
+for(image of allImages) {
+    image.addEventListener('dragend', (e) => {
+        console.log(e.clientX, e.clientY);
+        console.log(draggedImage);
+    
+
+        // Maintenant : récupérer l'id de l'image de la zone de drop
+        for(image of allImages) {
+            if(e.clientX >= image.x && e.clientX <= image.x + image.width && e.clientY >= image.y && e.clientY <= image.y + image.height) {
+                targetImage = image;
+                // log de vérif
+                console.log(targetImage);
+            }
+        }
+
+        // targetImage prend maintenant le src de draggedImage
+        let tmp = targetImage.src;
+        targetImage.setAttribute('src', draggedImage.getAttribute('src'));
+
+        // et targetImage prend le src de draggedImage.
+        draggedImage.setAttribute('src', tmp);
+    });
+}
+
+
+
  
 // FIN
